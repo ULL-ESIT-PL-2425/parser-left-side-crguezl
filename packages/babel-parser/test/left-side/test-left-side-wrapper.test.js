5959e1d62e65b1c18a1dbe8e2a71471d68b28b8d
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2024-12-24 19:08:52 +0000

    test for left-side not working

--- packages/babel-parser/test/left-side/test-left-side-wrapper.test.js
+++ packages/babel-parser/test/left-side/test-left-side-wrapper.test.js
@@ -3,17 +3,27 @@ const fs = require("fs");
 const path = require("path");
 const input = fs.readdirSync(path.join(__dirname, "in"), {encoding:"utf-8"});
 const execOut = new Set(fs.readdirSync(path.join(__dirname, "exec_out"), {encoding:"utf-8"}));
+let configFile = path.join(__dirname, "babel.config.js");
 
 for (let testFile of input) {
   test(testFile, () => {
     let fullPath = path.join(__dirname, "in", testFile);
-    let configFile = path.join(__dirname, "babel.config.js");
-    console.log(fullPath);
-    expect(execOut.has(testFile)).toBeTruthy();
-    const output = execSync(`npx babel --config-file ${configFile} ${fullPath}`);
-    const outputPath = path.join(__dirname, "out", testFile);
-    fs.writeFileSync(outputPath, output, {encoding: "utf-8"});
-    let execResult = execSync(`node ${outputPath}`);
-    expect(execResult).toBe(fs.readFileSync(`./exec_out/${testFile}`, {encoding: "utf-8"}));
+    console.log(`Processing input program "${fullPath}"`);
+
+    expect(execOut.has(testFile)).toBeTruthy(); // Check that the output file exists in the exec_out folder
+
+    // Compile with the left-side babel parser
+    const output = execSync(`npx babel --config-file ${configFile} ${fullPath}`).toString();
+    const outputPath = path.join(__dirname, "out", testFile); 
+    fs.writeFileSync(outputPath, output, {encoding: "utf-8"}); // Save the output program in out/
+
+    // Execute the compiled program
+    let execResult = execSync(`node ${outputPath}`).toString().replace(/\s+/g,"");
+  
+    // Compare the output with the expected output
+    const execPath = path.join(__dirname, "exec_out", testFile); 
+    const expectedResult = fs.readFileSync(execPath, {encoding: "utf-8"}).replace(/\s+/g,"");
+    console.log(`execResult = "${execResult}" ${typeof execResult} ${execResult.length} expectedResult = "${expectedResult}" ${expectedResult.length} ${typeof execResult} ${expectedResult === execResult}`);
+    expect(execResult).toEqual(expectedResult);
   })
 }

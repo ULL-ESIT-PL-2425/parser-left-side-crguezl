52189ff29829357ac7af38dabaa26d8d6f9c970c
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2024-12-25 12:05:39 +0000

    save

--- packages/babel-parser/test/left-side/left-side.js
+++ packages/babel-parser/test/left-side/left-side.js
@@ -4,7 +4,7 @@ function getParser(code) {
   return () => parse(code, { sourceType: "module" });
 }
 
-describe("left side function assignment", function () {
+describe("left-side function assignment", function () {
   it("should parse", function() {
     let ast = parse("foo(5) = 10");
     expect(ast.program.type).toEqual("Program");
--- packages/babel-parser/test/left-side/test-left-side-wrapper.test.js
+++ packages/babel-parser/test/left-side/test-left-side-wrapper.test.js
@@ -1,34 +1,36 @@
 const { execSync } = require("child_process");
 const fs = require("fs");
 const path = require("path");
-const input = fs.readdirSync(path.join(__dirname, "in"), {encoding:"utf-8"});
-const execOut = new Set(fs.readdirSync(path.join(__dirname, "exec_out"), {encoding:"utf-8"}));
+const input = fs.readdirSync(path.join(__dirname, "in"), { encoding: "utf-8" });
+const execOut = new Set(fs.readdirSync(path.join(__dirname, "exec_out"), { encoding: "utf-8" }));
 let configFile = path.join(__dirname, "babel.config.js");
 const ansiRegex = /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/ug;
 
 for (let testFile of input) {
-  test(testFile, () => {
-    let fullPath = path.join(__dirname, "in", testFile);
-    //console.log(`Processing input program "${fullPath}"`);
+  describe("left-side", () => {
+    test("left side function assignment " + testFile, () => {
+      let fullPath = path.join(__dirname, "in", testFile);
+      //console.log(`Processing input program "${fullPath}"`);
 
-    expect(execOut.has(testFile)).toBeTruthy(); // Check that the output file exists in the exec_out folder
+      expect(execOut.has(testFile)).toBeTruthy(); // Check that the output file exists in the exec_out folder
 
-    // Compile with the left-side babel parser
-    const output = execSync(`npx babel --config-file ${configFile} ${fullPath}`).toString();
-    const outputPath = path.join(__dirname, "out", testFile); 
-    fs.writeFileSync(outputPath, output, {encoding: "utf-8"}); // Save the output program in out/
+      // Compile with the left-side babel parser
+      const output = execSync(`npx babel --config-file ${configFile} ${fullPath}`).toString();
+      const outputPath = path.join(__dirname, "out", testFile);
+      fs.writeFileSync(outputPath, output, { encoding: "utf-8" }); // Save the output program in out/
 
-    // Execute the compiled program
-    let execResult = execSync(`node ${outputPath}`).toString().replace(ansiRegex,"").replace(/\s+/uig,"");
-  
-    // Compare the output with the expected output
-    const execPath = path.join(__dirname, "exec_out", testFile); 
-    const expectedResult = fs.readFileSync(execPath, {encoding: "utf-8"}).toString().replace(/\s+/uig,"");
-    
-/*    console.log(`\nexecResult is "${execResult}" ${typeof execResult} length=${execResult.length}
-expectedResult is "${expectedResult}" length=${expectedResult.length} ${typeof execResult} 
-${expectedResult[0] === execResult[0] ? "First characters are equal" : "First characters are different"}`);
-*/
-    expect(execResult).toEqual(expectedResult);
+      // Execute the compiled program
+      let execResult = execSync(`node ${outputPath}`).toString().replace(ansiRegex, "").replace(/\s+/uig, "");
+
+      // Compare the output with the expected output
+      const execPath = path.join(__dirname, "exec_out", testFile);
+      const expectedResult = fs.readFileSync(execPath, { encoding: "utf-8" }).toString().replace(/\s+/uig, "");
+
+      /*    console.log(`\nexecResult is "${execResult}" ${typeof execResult} length=${execResult.length}
+      expectedResult is "${expectedResult}" length=${expectedResult.length} ${typeof execResult} 
+      ${expectedResult[0] === execResult[0] ? "First characters are equal" : "First characters are different"}`);
+      */
+      expect(execResult).toEqual(expectedResult);
+    })
   })
 }

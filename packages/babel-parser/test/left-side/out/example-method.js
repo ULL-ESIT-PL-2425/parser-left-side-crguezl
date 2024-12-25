29d9d3e617b3bbe5fcb1c01a70e3e664030e85ad
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2024-12-25 12:34:37 +0000

    save

---
+++ packages/babel-parser/test/left-side/out/example-method.js
@@ -0,0 +1,13 @@
+const {
+  assign,
+  functionObject
+} = require("@ull-esit-pl/babel-plugin-left-side-support");
+let a = {
+  foo: functionObject(function (a) {
+    return a;
+  })
+};
+assign(a.foo, ["bar"], "fighter");
+console.log(a.foo("x")); // x
+console.log(a.foo("bar")); // fighter
+

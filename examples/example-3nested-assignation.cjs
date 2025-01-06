7c6fae598c2df222a47e20fafe4f34739b0f58ff
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2025-01-06 08:09:28 +0000

    considering new translation scheme: see translation-goal-example-nested-assignation.cjs

--- examples/example-3nested-assignation.cjs
+++ examples/example-3nested-assignation.cjs
@@ -9,9 +9,10 @@ const foo = functionObject(function (a) {
     });
   });
 });
-
+// Translation scheme for foo(2)(3)(5) = "Another value";
 assign(foo, [2], foo(2)),
 assign(foo(2), [3], foo(2)(3)),
 assign(foo(2)(3), [5], "Another value");
-console.log(foo(2)(3)(5)); // "Another Value". Currently returning 8.
+
+console.log(foo(2)(3)(5)); // "Another Value". 
 console.log(foo(2)(3)(6)); // 11

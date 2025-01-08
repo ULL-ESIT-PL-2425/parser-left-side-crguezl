10e8d0cac82ada181e78c8dba7d4678598f681e0
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2025-01-08 20:25:48 +0000

    discovered and fixed a bug in packages/babel-plugin-left-side-support/src/function-object.js checking undefined!:  if (this?.cache && this.cache.get(arg) !== undefined) {

--- examples/array.js
+++ examples/array.js
@@ -1,11 +1,13 @@
 // Arrays
 let a = functionObject([1,2,3]); // Potential Syntax @@ [1,2,3]
-console.log(a(0)); // 1
-console.log(a(2)); // 3
-console.log(a(3)); // undefined
+console.log(a(0));  // 1
+console.log(a(2));  // 3
+console.log(a(3));  // undefined
 console.log(a(-1)); // 3
-console.log(a("length")); // 1
+console.log(a("length")); // 1 number of arguments
 a(0) = -3;
-console.log(a(0)); // -3
-console.log(a(2)); // 3
+console.log(a(0));   // -3
+a(2) = 4;
+console.log(a(2));   // 4
+console.log(a.size); // 2
 
--- examples/object.js
+++ examples/object.js
@@ -1,7 +1,7 @@
 // Objects
 let ob = functionObject({x: 4, y: 5});
-console.log(ob("x")); 
-console.log(ob("y"));
+console.log(ob("x")); // 4 
+console.log(ob("y")); // 5
 ob("z") = 0;
-console.log(ob instanceof FunctionObject);
-console.log(ob("z"));
+console.log(ob instanceof FunctionObject); // true
+console.log(ob("z")); // 0

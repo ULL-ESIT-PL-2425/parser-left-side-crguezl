fb977ea1445f2815dd84ff6f5ececb82553334d3
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2025-01-09 10:17:02 +0000

    Thinking on a depth restructuring of the FunctionObject class

--- examples/array.js
+++ examples/array.js
@@ -7,7 +7,7 @@ console.log(a(-1)); // 3
 console.log(a("length")); // 1 number of arguments
 a(0) = -3;
 console.log(a(0));   // -3
-a(2) = 4;
-console.log(a(2));   // 4
+a(2) = 0;
+console.log(a(2));   // 0
 console.log(a.size); // 2
 

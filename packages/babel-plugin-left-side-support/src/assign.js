4ebd6792ffca6fdbbf3477f4c82dc25f1ed1bbf0
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2025-01-10 12:08:10 +0000

    removed the old versions of function-object (manyargs and single-argument)

--- packages/babel-plugin-left-side-support/src/assign.js
+++ packages/babel-plugin-left-side-support/src/assign.js
@@ -1,14 +1,15 @@
-const { functionObject } = require("./function-object");
+const { functionObject,  } = require("./function-object");
 
 function assign(f, cacheArgs, cacheValue) {
   //console.log('src f', f.toString(), cacheArgs, cacheValue);
   //debugger;
   
-  const cacheArgument = cacheArgs[0];
-  if (!f.cache) {
-     // warning f pointer modification
-     f = functionObject(f);
+  if (!(f instanceof FunctionObject)) {
+     // TODO: move this conditional if !(...) error to the plugin before assignment and show the line number 
+     throw `TypeError: Assigning to an ordinary Function. Convert to FunctionObject instead.`;
+     f = functionObject(f); // TODO: Why is that if the throw is removed, this line does not work? Because is a "value assignment" and the true "f" is not changed? It must be done at compile time?
   }
+  const cacheArgument = cacheArgs[0];
   f.setCache(cacheArgument, cacheValue);
   return cacheValue; 
 }

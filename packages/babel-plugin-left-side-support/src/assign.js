22bd617efa7d20d9665a3394d745237bc1381dd8
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2025-01-10 13:13:56 +0000

    packages/babel-plugin-left-side-support/src/function-object.js

--- packages/babel-plugin-left-side-support/src/assign.js
+++ packages/babel-plugin-left-side-support/src/assign.js
@@ -1,4 +1,4 @@
-const { functionObject,  } = require("./function-object");
+const { functionObject,  FunctionObject} = require("./function-object");
 
 function assign(f, cacheArgs, cacheValue) {
   //console.log('src f', f.toString(), cacheArgs, cacheValue);
@@ -7,7 +7,7 @@ function assign(f, cacheArgs, cacheValue) {
   if (!(f instanceof FunctionObject)) {
      // TODO: move this conditional if !(...) error to the plugin before assignment and show the line number 
      throw `TypeError: Assigning to an ordinary Function. Convert to FunctionObject instead.`;
-     f = functionObject(f); // TODO: Why is that if the throw is removed, this line does not work? Because is a "value assignment" and the true "f" is not changed? It must be done at compile time?
+     // f = functionObject(f); // TODO: Why is that if the throw is removed, this line does not work? Because is a "value assignment" and the true "f" is not changed? It must be done at compile time?
   }
   const cacheArgument = cacheArgs[0];
   f.setCache(cacheArgument, cacheValue);

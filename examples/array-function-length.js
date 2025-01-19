5eb5446a57048e583d46a702ea3d10657b9e52b2
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2025-01-19 14:16:36 +0000

    save

--- examples/array-function-length.js
+++ examples/array-function-length.js
@@ -1,10 +1,9 @@
-const b = [ 3, 2, 1 ];
 let a = functionObject(
-  b, {
+  [3, 2, 1], {
     debug: false,
-    exception: (x, e) => { 
-      if ((typeof x == "string") && b[x] !== undefined) {
-        return b[x];
+    exception: (x, e, primitive) => { 
+      if ((typeof x == "string") && primitive[x] !== undefined) {
+        return primitive[x];
       } else throw e;
   }}
 ); 
@@ -12,6 +11,6 @@ a(-1) = 2;
 console.log(a(-1)); // 2
 console.log(a("length")); // 3
 a(3) = 4;
-console.log(a("length")); // 3 since "b" has not changed
+console.log(a("length")); // 3 since "primitive" has not changed
 console.log(a.cache.size); // 2
 console.log(a.cache.toString()); // [[-1,2],[3,4]]

810250bbfa7ae37bee211a4e91698ce53bbda8fe
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2025-01-19 11:07:53 +0000

    See packages/babel-plugin-left-side-support/examples/proposal-with-ids.cjs. working on an efficient translation of foo(2)(3)(5) = "another"

--- packages/babel-plugin-left-side-support/examples/proposal-with-ids.cjs
+++ packages/babel-plugin-left-side-support/examples/proposal-with-ids.cjs
@@ -15,11 +15,11 @@ const foo = functionObject(function foo(a) {
 // It is urgent to optimize the assign function!!!!
 // TODO: foo(2)(3) is computed twice! 
 // More efficient translation of foo(2)(3)(5) = "Another value"
-let uid1, uid2; // Declare n-1 variables with unique identifiers in the local scope
+let uid3, uid2, uid1; // Declare n-1 variables with unique identifiers in the local scope
 
-assign(foo, [2], uid1 = foo(2)),              // uid1 is a function
-assign(uid1, [3], uid2 = uid1(3)),            // Save the function uid2=foo(2)(3) in the cache of uid1 = foo(2)
-assign(uid2, [5], "Another value");
+assign(foo, [2], uid3 = foo(2)),              // uid3 is a function
+assign(uid3, [3], uid2 = uid3(3)),            // Save the function uid2=foo(2)(3) in the cache of uid3 = foo(2) for the entry 3
+assign(uid2, [5], uid1 = "Another value");    // Save the value "Another value" in the cache of uid2 = foo(2)(3) for the entry 5
 console.log(foo(2)(3)(5)); // "Another Value"
 console.log(foo(2)(3)(6)); // 11
 console.log(foo(1)(3)(6)); // 10

2838a4883e37ee6b63826e562f7ac3d65e96a959
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2025-01-21 18:27:25 +0000

    save

--- examples/not-working/multiparameter/example-multiparameter.js
+++
@@ -1,9 +0,0 @@
-function @@ foo(bar, x) {
-    return bar + x;
-}
-
-foo("one", "two") = null;
-
-console.log(foo(1,2)); // 3
-console.log(foo("grass", "hopper")); // grasshopper
-console.log(foo("one", "two")); // null

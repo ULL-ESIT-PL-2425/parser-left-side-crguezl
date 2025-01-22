59485a22a6ecae762da457b4928142e0e973eaf2
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2025-01-22 10:50:24 +0000

    discussing

--- packages/babel-parser/test/left-side/in/many-parameters.js
+++ packages/babel-parser/test/left-side/in/many-parameters.js
@@ -1,8 +1,7 @@
-// May be in the future this will be a synonim of foo(a)(b) but by now let us throw an error
-function @@ foo(a, b) {
+function @@ foo(a, b) { // foo gets currified automatically
   return a+b;
 }
 
-foo(2)(3) = 5; // The call f(2,5) is not yet implemented
+foo(2)(3) = 1;  // TODO: Make foo(2,3) = 1 work
 console.log(foo(2)(3)); // 5
 console.log(foo(2)(5)); // 7
--- packages/babel-parser/test/left-side/out/many-parameters.js
+++ packages/babel-parser/test/left-side/out/many-parameters.js
@@ -5,10 +5,10 @@ const {
   FunctionObject,
   Storage
 } = require("@ull-esit-pl-2425/babel-plugin-left-side-support");
-// May be in the future this will be a synonim of foo(a)(b) but by now let us throw an error
 const foo = functionObject(function foo(a, b) {
+  // foo gets currified automatically
   return a + b;
 });
-mAssign(foo, [2, 3], 5); // The call f(2,5) is not yet implemented
+mAssign(foo, [2, 3], 1); // TODO: Make foo(2,3) = 1 work
 console.log(foo(2)(3)); // 5
 console.log(foo(2)(5)); // 7

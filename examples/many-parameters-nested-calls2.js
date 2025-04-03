6a1e7c96afe09de8568b077103cf85993a34ff24
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2025-04-03 09:53:51 +0100

    added many-parameters-nested-calls2.js with @@

--- examples/many-parameters-nested-calls2.js
+++ examples/many-parameters-nested-calls2.js
@@ -1,7 +1,7 @@
 // May be in the future this will be a synonim of foo(a)(b) but by now let us throw an error
-let foo = new FunctionObject(function(a, b) {
+function @@ foo(a, b) {
   return a+b;
-});
+}
 
 foo(2)(3) = 1; // TODO: The call f(2,3) is implemented via massign
 console.log(foo(2)(3)); // 1

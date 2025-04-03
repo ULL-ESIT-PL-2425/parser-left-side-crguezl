a182e6bab1b70f6f6a107d0d56154253adc6145d
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2025-04-03 09:17:23 +0100

    comment in many-parameters

--- examples/many-parameters.js
+++ examples/many-parameters.js
@@ -3,5 +3,5 @@ function @@ foo(a, b) { // foo gets currified automatically
 }
 
 foo(2)(3) = 1;  // TODO: Make foo(2,3) = 1 work
-console.log(foo(2)(3)); // 5
+console.log(foo(2)(3)); // 1
 console.log(foo(2)(5)); // 7
